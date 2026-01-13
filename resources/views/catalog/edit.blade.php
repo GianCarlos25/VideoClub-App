f9ea1a40b52058ff8dbd9a24c1d93b4e30df38e6
Gian <giancarlos.sh25@gmail.com>
2026-01-13 16:36:16 +0100

    Subiendo proyecto Videoclub Laravel

---
+++ resources/views/catalog/edit.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+
+@section('content')
+Modificar película {{ $id }}
+@stop
---
+++ resources/views/catalog/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+
+@section('content')
+Vista detalle película {{ $id }}
+@stop
